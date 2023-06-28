160deefd057db8ef8fc5b6108ea6fa5a1b086e2e
yeferson <yefersonapk@gmail.com>
2023-06-27 19:45:16 -0500

    right 9

--- api/index.js
+++ api/index.js
@@ -26,12 +26,12 @@ const options = {
 app.use(cors(options));
 
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
     res.send('Hello World');
 });
 
 
-app.get('/nueva-ruta', (req, res) => {
+app.get('api/nueva-ruta', (req, res) => {
     res.send('Hola, soy una nueva ruta');
 });
 
